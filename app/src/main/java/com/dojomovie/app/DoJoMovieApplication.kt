1d4a768a5be896f849239936db66f70e66c6e274
Ash <ghtmarco@gmail.com>
2025-05-06 15:33:39 +0700

    Create DoJoMovieApplication class

---
+++ app/src/main/java/com/dojomovie/app/DoJoMovieApplication.kt
@@ -0,0 +1,10 @@
+package com.dojomovie.app
+
+import android.app.Application
+
+class DoJoMovieApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Any global initialization can be done here
+    }
+}
