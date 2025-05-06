aacda6c5161fbe92ab9cc7bed52c39206ed9104d
Ash <ghtmarco@gmail.com>
2025-05-06 15:31:43 +0700

    Create MainActivity with bottom navigation

---
+++ app/src/main/java/com/dojomovie/app/activities/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.dojomovie.app.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.dojomovie.app.R
+import com.dojomovie.app.databinding.ActivityMainBinding
+import com.dojomovie.app.fragments.HomeFragment
+import com.dojomovie.app.fragments.HistoryFragment
+import com.dojomovie.app.fragments.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        setupBottomNavigation()
+        
+        // Set default fragment
+        if (savedInstanceState == null) {
+            loadFragment(HomeFragment())
+            binding.bottomNavigation.selectedItemId = R.id.nav_home
+        }
+    }
+    
+    private fun setupBottomNavigation() {
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.nav_history -> {
+                    loadFragment(HistoryFragment())
+                    true
+                }
+                R.id.nav_profile -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+    
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
