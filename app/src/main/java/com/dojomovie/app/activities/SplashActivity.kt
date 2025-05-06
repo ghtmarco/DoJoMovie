1f436c88a451858f16db3a9c80f682fce31c54be
Ash <ghtmarco@gmail.com>
2025-05-06 15:30:27 +0700

    Create SplashActivity

---
+++ app/src/main/java/com/dojomovie/app/activities/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.dojomovie.app.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.dojomovie.app.R
+import com.dojomovie.app.utils.SessionManager
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var sessionManager: SessionManager
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        
+        sessionManager = SessionManager(this)
+        
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = if (sessionManager.isLoggedIn()) {
+                Intent(this, MainActivity::class.java)
+            } else {
+                Intent(this, LoginActivity::class.java)
+            }
+            startActivity(intent)
+            finish()
+        }, 2000) // 2 second delay
+    }
+}
